2fb17ba59c5350f5f89e2e023bce8a124857d432
Michael Belozerskiy <m.belozerskiy@yandex.ru>
2018-02-28 01:43:39 +0300

    create StudentCreate component

---
+++ src/pages/StudentCreate.vue
@@ -0,0 +1,24 @@
+<template>
+  <stepper :steps="steps"></stepper>
+</template>
+
+<script>
+import Stepper from '@/components/Reusable/Stepper'; 
+import PersonalInfo from '@/components/StudentSteps/PersonalInfo'; 
+import PersonalCourse from '@/components/StudentSteps/PersonalCourse';
+import PersonalSchedule from '@/components/StudentSteps/PersonalSchedule';
+
+export default {
+  name: 'StudentCreate',
+  components: { Stepper },
+  data(){
+    return {
+      steps: [
+        { name: 'Основная информация', component: PersonalInfo },
+        { name: 'Индивидуальная программа', component: PersonalCourse },
+        { name: 'Расписание', component: PersonalSchedule }
+      ]
+    }
+  },
+}
+</script>
