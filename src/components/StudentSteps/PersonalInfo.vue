da5e2a1ac0b42a07536a0f11ce3c40cd7ca9085a
Michael Belozerskiy <m.belozerskiy@yandex.ru>
2018-02-28 01:43:20 +0300

    add steps component for create user component

---
+++ src/components/StudentSteps/PersonalInfo.vue
@@ -0,0 +1,18 @@
+<template>
+  <panel>
+    <panel-body>
+      <personal-info-form></personal-info-form>
+    </panel-body>
+  </panel>
+</template>
+
+<script>
+import Panel from '@/components/Reusable/Panel';
+import PanelBody from '@/components/Reusable/PanelBody';
+import PersonalInfoForm from '@/components/Forms/PersonalInfoForm';
+
+export default {
+  name: 'PersonalInfo',
+  components: { Panel, PanelBody, PersonalInfoForm }
+}
+</script>
