1fe995ac259c242add0bd183dd15124cb9a66c12
Michael Belozerskiy <m.belozerskiy@yandex.ru>
2018-02-28 15:10:30 +0300

    Add new Input to PersonalInfoForm

--- src/components/Forms/PersonalInfoForm.vue
+++ src/components/Forms/PersonalInfoForm.vue
@@ -1,7 +1,7 @@
 <template>
   <form>
     <div class="form-group required">
-      <label class="control-label" for="phone">Телефон</label>
+      <label class="control-label" for="phone">Телефон:</label>
       <input 
         v-model="phone"
         v-mask="phoneMask" 
@@ -9,13 +9,23 @@
         class="form-control" 
         id="phone" 
         name="phone"
-        required="required"
         placeholder="+7(999)111-11-11"
       >
     </div>
 
-    <div class="form-group">
-      <label for="fullname">ФИО</label>
+    <div class="form-group required">
+      <label class="control-label" for="password">Пароль:</label>
+      <input 
+        :type="showPassword ? 'text' : 'password'" 
+        class="form-control" 
+        id="password"
+        v-model="password" 
+        placeholder="Сусанин Игорь Иванович"
+      >
+    </div>
+
+    <div class="form-group required">
+      <label class="control-label" for="fullname">ФИО:</label>
       <input 
         type="text" 
         class="form-control" 
@@ -24,7 +34,41 @@
         placeholder="Сусанин Игорь Иванович"
       >
     </div>
+
+    <div class="form-group required">
+      <label class="control-label" for="email">Электронная почта:</label>
+      <input 
+        type="text" 
+        class="form-control" 
+        id="email"
+        v-model="email" 
+        placeholder="igorIvanonich96@mail.ru"
+      >
+    </div>
     
+    <div class="form-group required">
+      <label class="control-label" for="fullname">Цель:</label>
+      <input 
+        type="text" 
+        class="form-control" 
+        id="fullname"
+        v-model="fullname" 
+        placeholder="Сусанин Игорь Иванович"
+      >
+    </div>
+
+    <div class="form-group required">
+      <label class="control-label" for="class">Класс:</label>
+      <input 
+        type="text" 
+        class="form-control" 
+        id="class"
+        v-model="classes" 
+        placeholder="Сусанин Игорь Иванович"
+      >
+    </div>
+    
+    <pre>{{ $v }}</pre>
   </form>
 </template>
 <script>
@@ -35,9 +79,19 @@ export default {
   name: "PersonalInfoForm",
   data(){
     return {
+      form: [
+        {},
+        {},
+        {}
+      ],
       phone: "",
       phoneMask: "+7(###)###-##-##",
       fullname: "",
+      password: "",
+      showPassword: false,
+      email: "",
+      classes: "",
+      target: ""
     }
   },
   validations(){
