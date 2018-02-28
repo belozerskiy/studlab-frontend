2155ce724b207d6e0828ab9ce712fc9e6b1c125e
Michael Belozerskiy <m.belozerskiy@yandex.ru>
2018-02-28 14:37:29 +0300

    add validators in PersonalInfoForm

--- src/components/Forms/PersonalInfoForm.vue
+++ src/components/Forms/PersonalInfoForm.vue
@@ -15,29 +15,37 @@
     </div>
 
     <div class="form-group">
-      <label for="exampleInputEmail1">ФИО</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+      <label for="fullname">ФИО</label>
+      <input 
+        type="text" 
+        class="form-control" 
+        id="fullname"
+        v-model="fullname" 
+        placeholder="Сусанин Игорь Иванович"
+      >
     </div>
     
-    <div class="form-group">
-      <label for="exampleInputFile">File input</label>
-      <input type="file" id="exampleInputFile">
-      <p class="help-block">Example block-level help text here.</p>
-    </div>
-    <div class="checkbox">
-      <label>
-        <input type="checkbox"> Check me out
-      </label>
-    </div>
   </form>
 </template>
 <script>
+import { required } from 'vuelidate/lib/validators';
+import { isFullname } from '@/validators';
+
 export default {
   name: "PersonalInfoForm",
   data(){
     return {
       phone: "",
       phoneMask: "+7(###)###-##-##",
+      fullname: "",
+    }
+  },
+  validations(){
+    return {
+      fullname: {
+        required,
+        isFullname
+      }
     }
   }
 };
