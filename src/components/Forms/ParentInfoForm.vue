17983c5d296134d4d435f70900a162fb74be63a3
Michael Belozerskiy <m.belozerskiy@yandex.ru>
2018-02-28 15:10:15 +0300

    remove unusable input from ParentInfoForm

--- src/components/Forms/ParentInfoForm.vue
+++ src/components/Forms/ParentInfoForm.vue
@@ -24,11 +24,6 @@
       >
     </div>
     
-    <div class="form-group">
-      <label for="exampleInputFile">File input</label>
-      <input type="file" id="exampleInputFile">
-      <p class="help-block">Example block-level help text here.</p>
-    </div>
     <pre>{{ $v }}</pre>
   </form>
 </template>
